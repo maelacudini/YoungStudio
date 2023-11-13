1f844190a3fdb03c000ff40182cd982954aa92cf
maelacudini <maelacudini@gmail.com>
2023-11-13 17:12:10 +0100

    second commit

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -18,7 +18,7 @@ export default function Home() {
       <Title />
       <ParallaxGallery />
       <Projects />
-      <HorizontalScroll />
+      {/* <HorizontalScroll /> */}
     </motion.div>
   );
 }
